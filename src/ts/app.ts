e78a17250e44640bf0c197626f94a85fd5fdeae4
AhmedBHameed <ahmedbazy@gmail.com>
2017-06-20 12:44:56 +0200

    Enhancement

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,7 +2,6 @@ declare var require: any;
 var style = require('./../sass/style.scss'); 		// this is main css file to bundle (try to include any sass file inside to make it one file as destination);
 import { sayHi } from './otherClasses';
 
-
 let dependency:any = new sayHi();
 let h1:any = document.createElement('h1');
 h1.innerHTML = dependency.sayHi('from Webpack pachage.');
